632c60977f9d824da9039adc1c0507c6ebb9e46e
Eku <tsumit665420@gmail.com>
2025-06-29 10:57:43 +0000

    import { useEffect, useState } from "react"; import { motion, AnimatePre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import ScrollToTop from '@/components/ScrollToTop';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -39,6 +40,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <Toaster />
+        <ScrollToTop />
       </body>
     </html>
   );
