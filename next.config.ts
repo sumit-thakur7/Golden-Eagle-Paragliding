6890690d62d3c6332cd1fa6a0690051a328e0d10
ravi296 <ravi.thakur296@gmail.com>
2025-07-12 23:36:46 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -87,3 +87,9 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+module.exports = nextConfig;
